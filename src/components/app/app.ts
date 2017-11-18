4cd9850777b6d45e3d70f806af37b69b7e0f792c
lahode <lahode@infomaniak.ch>
2017-11-18 15:23:35 +0100

    Activate lazyloading on new group route

--- src/components/app/app.ts
+++ src/components/app/app.ts
@@ -1,8 +1,5 @@
 import Vue from 'vue';
 import Component from 'vue-class-component';
-import { HomeComponent } from '../home';
-import { GroupComponent } from '../group';
-import { NewGroupComponent } from '../new-group';
 import { MenuComponent } from '../menu/menu';
 
 import './app.scss';
@@ -10,7 +7,6 @@ import './app.scss';
 @Component({
     template: require('./app.html'),
     components: {
-      appHome : NewGroupComponent,
       appMenu : MenuComponent
     }
 })
--- src/router.ts
+++ src/router.ts
@@ -6,8 +6,13 @@ import { Group } from './models/group.model';
 import { User } from './models/user.model';
 
 const homeComponent = () => import('./components/home').then(({ HomeComponent }) => HomeComponent);
-const newGroupComponent = () => import('./components/new-group').then(({ NewGroupComponent }) => NewGroupComponent);
 const groupComponent = () => import('./components/group').then(({ GroupComponent }) => GroupComponent);
+// Charge New group component en lazy loading
+const newGroupComponent = resolve => {
+  require.ensure(['./components/new-group'], () => {
+    resolve((<any>require('./components/new-group')).NewGroupComponent)
+  })
+}
 
 export const user1 = new User('soloh', 'Han', 'Solo');
 export const user2 = new User('doj', 'Jon', 'Do');
@@ -25,14 +30,14 @@ export const groups = [
 
 if (process.env.ENV === 'development' && module.hot) {
   const homeModuleId = './components/home';
-  const newGroupModuleId = './components/new-group';
+  // const newGroupModuleId = './components/new-group';
   const groupModuleId = './components/group';
 
   makeHot(homeModuleId, homeComponent,
     module.hot.accept('./components/home', () => reload(homeModuleId, (<any>require('./components/home')).HomeComponent)));
 
-  makeHot(newGroupModuleId, newGroupComponent,
-    module.hot.accept('./components/new-group', () => reload(newGroupModuleId, (<any>require('./components/new-group')).NewGroupComponent)));
+  // makeHot(newGroupModuleId, newGroupComponent,
+  //  module.hot.accept('./components/new-group', () => reload(newGroupModuleId, (<any>require('./components/new-group')).NewGroupComponent)));
 
   makeHot(groupModuleId, groupComponent,
     module.hot.accept('./components/group', () => reload(groupModuleId, (<any>require('./components/group')).GroupComponent)));
